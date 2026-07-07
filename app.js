9a095e0773b82be30898b779b6eac6b62f91f284
prsd503 <brink2wink@gmail.com>
2026-07-07 16:12:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBEYKHQpy_VjmgjYiWQOPjXth1bghYsf9M",
+    apiKey: "AIzaSyBEYKHQpy_VjmgjYiWQQPjXth1bghYsf9M",
     authDomain: "finder-owl.firebaseapp.com",
     projectId: "finder-owl",
-    storageBucket: "finder-owl.firebasestorage.app",
-    messagingSenderId: "1011347100861",
+    storageBucket: "finder-owl.appspot.com",
     appId: "1:1011347100861:web:24246f9a4eb24d812cd3d4"
 };
 
 const app = initializeApp(firebaseConfig);
-
-// EXPORTS: Critical for fixing the "binding name not found" error
-export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
