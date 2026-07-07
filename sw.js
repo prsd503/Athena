e37a1eeb3240a897def8f18edddf54a67bf95b4e
prsd503 <brink2wink@gmail.com>
2026-07-07 17:47:54 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('finder-owl-v1').then(cache => cache.addAll(['/', '/index.html', '/manifest.json', '/app.js'])));
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
