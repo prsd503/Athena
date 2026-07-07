5bbea659bb0872af08271e3656782e3667b085d0
prsd503 <brink2wink@gmail.com>
2026-07-07 16:34:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBEYKHQpy_VjmgjYIwQOPjXth1bghYsf9M",
-    authDomain: "finder-owl.firebaseapp.com",
-    projectId: "finder-owl",
-    storageBucket: "finder-owl.firebasestorage.app",
-    messagingSenderId: "1011347100861",
-    appId: "1:1011347100861:web:24246f9a4eb24d812cd3d4"
+  apiKey: "AizaSyBEYKHQpy_VjmgjYIwQOPjXth1bghYsf9M",
+  authDomain: "finder-owl.firebaseapp.com",
+  projectId: "finder-owl",
+  storageBucket: "finder-owl.firebasestorage.app",
+  messagingSenderId: "1011347100861",
+  appId: "1:1011347100861:web:24246f9a4eb24d812cd3d4",
+  measurementId: "G-6FWCNYMK8C"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Export instances to use in your other logic files
 export const db = getFirestore(app);
 export const auth = getAuth(app);
