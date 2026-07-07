427ac27ffd29c047af4341bb9445dd0dbf165a76
prsd503 <brink2wink@gmail.com>
2026-07-07 16:29:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBEYKHQpy_VjmgjYiWQQPjXth1bghYsf9M",
+    apiKey: "AIzaSyBEYKHQpy_VjmgjYIwQOPjXth1bghYsf9M",
     authDomain: "finder-owl.firebaseapp.com",
     projectId: "finder-owl",
-    storageBucket: "finder-owl.appspot.com",
+    storageBucket: "finder-owl.firebasestorage.app",
+    messagingSenderId: "1011347100861",
     appId: "1:1011347100861:web:24246f9a4eb24d812cd3d4"
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
